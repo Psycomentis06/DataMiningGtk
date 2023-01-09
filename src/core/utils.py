91218bb43e17ffb8568d8d33633f72da080db67f
Psycomentis06 <alibenamor30@gmail.com>
2023-01-09 23:02:05 +0100

    import modules

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,5 +1,6 @@
-import requests
+# import requests
 from gi.repository import GLib, Gtk, Gdk, GdkPixbuf
+import importlib
 
 def load_image_from_url(url: str):
     response = requests.get(url)
@@ -11,4 +12,15 @@ def load_image_from_url(url: str):
 
     Gtk.Image.new_from_pixbuf(loader.get_pixbuf())
 
-    
+def get_filename_from_path(path: str):
+    return path.split('/')[-1]
+
+def remove_extention(filename: str):
+    return filename.split('.')[0]
+
+def import_module_from_abs_path(module_name: str, module_path: str):
+    module_spec = importlib.util.spec_from_file_location(module_name, module_path)
+    module = importlib.util.module_from_spec(module_spec)
+    module_spec.loader.exec_module(module)
+    return module
+
